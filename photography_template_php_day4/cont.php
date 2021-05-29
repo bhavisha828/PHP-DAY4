5e85d2c78f1830c001b40cc8164d92cd3eed6ddb
NAYI BHAVISHA AMRISHBHAI <nayibhavi44@gmail.com>
2021-05-29 14:53:05 +0530

    v1
    php-internship-day4

---
+++ photography_template_php_day4/cont.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>nature </title>
+<link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+<script type="text/javascript" src="jquery/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="jquery/jquery.gallerax-0.2.js"></script>
+<style type="text/css">
+@import "gallery.css";
+</style>
+</head>
+<body>
+
+<div id="wrapper">
+	<div id="header-wrapper">
+        <?php include './include/menu.php'; ?>
+		<!-- end #header -->
+	</div>
+       
+
+	<div id="welcome">
+		<h2 class="title"><a>CONTACT </a></h2>
+		<div class="entry">
+			<p>nayibhavisha@gmail.com</p>
+			<p><a href="https://github.com/bhavisha828">https://github.com/bhavisha828</a></p>
+		</div>
+	
+	
+</div>
+
+<div id="footer">
+	<p>Manage by <a href="https://github.com/bhavisha828">@Bhavisha Nayi</a></p>
+</div>
+
+<!-- end #footer -->
+
+</body>
+</html>
